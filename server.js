cf9cc69b41dfae0c93a8e37959fda610eb1d1bef
Glitch (same-gold-pink) <none>
2024-01-02 18:51:50 +0000

    updated packet tracer file with (unfinished) real life connections

--- server.js
+++ server.js
@@ -4,16 +4,32 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { engine } from "express-handlebars";
 import { router } from "./routes.js";
+import jsonparser  from "jsonparser";
 
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json);
 app.use(express.static("public"));
 app.use(fileUpload());
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 app.use("/", router);
+
+//const jsonParser = bodyParser.json();
+app.get("/hi", function (request, response) {
+  console.log("hi");
+  response.send("hi");
+})
+app.get("/weathertop", function (request, response) {
+  console.log("weathertop received");
+  response.send("weathertop");
+})
+app.post("/weathertoplive", jsonparser, function(req, res) {
+  console.log("webhook received");
+  console.log(req.body);
+})
 
 const listener = app.listen(process.env.PORT || 4000, function () {
   console.log(
--- utils/smarthome-analytics.js
+++ utils/smarthome-analytics.js
@@ -2,22 +2,18 @@ import axios from "axios";
 
 const blynkAuth = 'Id8ho2TW5jHNcB384TY966cZO-DwlOVy';
 
-let blynkButton;
-let blynkTemp;
-let blynkPressure;
-let blynkHumid;
-
 export const smartHomeAnalytics = {
   async getAllBlynkReadings(){
     const getBlynkReadings = await axios.get(`https://fra1.blynk.cloud/external/api/getAll?token=${blynkAuth}`);
     console.log(getBlynkReadings.data);
-    smartHomeAnalytics.blynkStatus(getBlynkReadings.data.v0);
-    smartHomeAnalytics.blynkTemp(getBlynkReadings.data.v1);
-    blynkPressure = getBlynkReadings.data.v2;
-    blynkHumid = getBlynkReadings.data.v3;
+    const { blynkButton, refreshPage } = smartHomeAnalytics.blynkStatus(getBlynkReadings.data.v0);
+    const blynkTemp = smartHomeAnalytics.blynkTemp(getBlynkReadings.data.v1);
+    const blynkPressure = getBlynkReadings.data.v2;
+    const blynkHumid = getBlynkReadings.data.v3;
     const blynkResults = { 
       blynkRaw: getBlynkReadings.data, 
       button: blynkButton, 
+      refreshPage: refreshPage,
       temp: blynkTemp, 
       pressure: blynkPressure, 
       humidity: blynkHumid
@@ -28,15 +24,20 @@ export const smartHomeAnalytics = {
   },
   
   blynkStatus(blynkReading) {
+    let blynkButton;
+    let refreshPage;
     if (blynkReading == 0) {
       blynkButton = 'off';
+      refreshPage = false;
     } else {
       blynkButton = 'on';
+      refreshPage = true;
     }
-    return blynkButton;
+    return { blynkButton, refreshPage };
   },
   
   blynkTemp(blynkReading) {
-    blynkTemp = `${blynkReading} Celsius`
+    const blynkTemp = `${Math.round(blynkReading * 100) / 100} Celsius`;
+    return blynkTemp;
   }
 }
